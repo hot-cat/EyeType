76ee6421444cb03dfc7ae375be3b42d5b0644cb9
the hottest cat <68014914+hot-cat@users.noreply.github.com>
2023-01-13 23:19:41 +0200

    hopefully mostly done with face detection part

---
+++ tflite/src/main/java/com/example/android/camerax/tflite/faceDetection/FaceDetection.java
@@ -0,0 +1,88 @@
+package com.example.android.camerax.tflite.faceDetection;
+
+import com.example.android.camerax.tflite.tools.types.Detection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FaceDetection {
+    //used in decode boxes
+    float[] anchors = SSD.ssdGenerateAnchors();
+
+    public float[][][] decodeBoxes(float[][][] raw_boxes) {
+        // width == height so scale is the same across the board
+        // 128 got from face detection model input size
+        float scale = 128;
+        int num_points = raw_boxes[0][0].length / 2;
+        // scale all values (applies to positions, width, and height alike)
+        float[][][] boxes = new float[raw_boxes.length][raw_boxes[0].length][num_points*2];
+        for(int i = 0; i < raw_boxes.length; i++) {
+            for(int j = 0; j < raw_boxes[0].length; j++) {
+                for(int k = 0; k < num_points*2; k++) {
+                    boxes[i][j][k] = raw_boxes[i][j][k]/scale;
+                }
+            }
+        }
+        // adjust center coordinates and key points to anchor positions
+        for(int i = 0; i < boxes.length; i++) {
+            boxes[i][0][0] += this.anchors[0];
+            boxes[i][0][1] += this.anchors[1];
+        }
+        for(int i = 2; i < num_points; i++) {
+            for(int j = 0; j < boxes.length; j++) {
+                boxes[j][i][0] += this.anchors[0];
+                boxes[j][i][1] += this.anchors[1];
+            }
+        }
+        // convert x_center, y_center, w, h to xmin, ymin, xmax, ymax
+        for(int i = 0; i < boxes.length; i++) {
+            float[] center = new float[] {boxes[i][0][0], boxes[i][0][1]};
+            float[] half_size = new float[] {boxes[i][1][0]/2, boxes[i][1][1]/2};
+            boxes[i][0][0] = center[0] - half_size[0];
+            boxes[i][0][1] = center[1] - half_size[1];
+            boxes[i][1][0] = center[0] + half_size[0];
+            boxes[i][1][1] = center[1] + half_size[1];
+        }
+        return boxes;
+    }
+
+
+    private static final float RAW_SCORE_LIMIT = 80.0f;
+    public float[][][] getSigmoidScores(float[][][] raw_scores) {
+            for (int i = 0; i < raw_scores.length; i++) {
+                for (int j = 0; j < raw_scores[0].length; j++) {
+                    for (int k = 0; k < raw_scores[0][0].length; k++) {
+                        if (raw_scores[i][j][k] < -RAW_SCORE_LIMIT) {
+                            raw_scores[i][j][k] = -RAW_SCORE_LIMIT;
+                        } else if (raw_scores[i][j][k] > RAW_SCORE_LIMIT) {
+                            raw_scores[i][j][k] = RAW_SCORE_LIMIT;
+                        }
+                        raw_scores[i][j][k] = 1.0f / (1.0f + (float) Math.exp(-raw_scores[i][j][k]));
+                    }
+                }
+            }
+            return raw_scores;
+    }
+    float MIN_SCORE = 0.5f; // you can set this to whatever threshold you want
+
+    public List<Detection> convertToDetections(float[][][] boxes, float[][][] scores) {
+        List<Detection> detections = new ArrayList<Detection>();
+        for (int i = 0; i < boxes.length; i++) {
+            for (int j = 0; j < boxes[i].length; j++) {
+                if (scores[i][j][0] > MIN_SCORE && is_valid(boxes[i][j])) {
+                    detections.add(new Detection(boxes[i][j], scores[i][j][0]));
+                }
+            }
+        }
+        return detections;
+    }
+    private static boolean is_valid(float[] box) {
+        return box[1] > box[0];
+    }
+
+
+
+    public FaceDetection(){
+
+    }
+}
---
+++ tflite/src/main/java/com/example/android/camerax/tflite/faceDetection/SSD.java
@@ -0,0 +1,53 @@
+package com.example.android.camerax.tflite.faceDetection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class SSD {
+    public static final int numLayers = 4;
+    public static final int inputSizeHeight = 128;
+    public static final int inputSizeWidth = 128;
+    public static final float anchorOffsetX = 0.5f;
+    public static final float anchorOffsetY = 0.5f;
+    public static final int[] strides = {8, 16, 16, 16};
+    public static final float interpolatedScaleAspectRatio = 1.0f;
+
+    public static float[] ssdGenerateAnchors() {
+        List<Float> anchors = new ArrayList<>();
+        int layerId = 0;
+        int numLayers = SSD.numLayers;
+        int[] strides = SSD.strides;
+        assert strides.length == numLayers;
+        float anchorOffsetX = SSD.anchorOffsetX;
+        float anchorOffsetY = SSD.anchorOffsetY;
+        float interpolatedScaleAspectRatio = SSD.interpolatedScaleAspectRatio;
+        while (layerId < numLayers) {
+            int lastSameStrideLayer = layerId;
+            int repeats = 0;
+            while (lastSameStrideLayer < numLayers && strides[lastSameStrideLayer] == strides[layerId]) {
+                lastSameStrideLayer++;
+                repeats += 2;
+            }
+            int stride = strides[layerId];
+            int featureMapHeight = SSD.inputSizeHeight / stride;
+            int featureMapWidth = SSD.inputSizeWidth / stride;
+            for (int y = 0; y < featureMapHeight; y++) {
+                float yCenter = (y + anchorOffsetY) / featureMapHeight;
+                for (int x = 0; x < featureMapWidth; x++) {
+                    float xCenter = (x + anchorOffsetX) / featureMapWidth;
+                    for (int i = 0; i < repeats; i++) {
+                        anchors.add(xCenter);
+                        anchors.add(yCenter);
+                    }
+                }
+            }
+            layerId = lastSameStrideLayer;
+        }
+        float[] anchorArray = new float[anchors.size()];
+        for (int i = 0; i < anchors.size(); i++) {
+            anchorArray[i] = anchors.get(i);
+        }
+        return anchorArray;
+    }
+
+}
